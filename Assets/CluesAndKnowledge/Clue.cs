cab4095d63c53b4159ee44bcd2193dfaca2e7b22
terapter <Terapter@users.noreply.github.com>
2019-05-25 22:56:54 +0100

    Start of ClueFactory and Clue extension

--- Assets/CluesAndKnowledge/Clue.cs
+++ Assets/CluesAndKnowledge/Clue.cs
@@ -1,11 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static ClueFactory;
 
 public class Clue : Knowledge
 {
+
+    ClueTypes clueType;
+    Feature feature;
+    //Clue needs a ClueTypes
+    //For Useless, we just need a string, we don't need to add to notebook
+    //For Vague, we need a single aspect of a feature
+    //For Unsure, we need an either/or
+    //For perfect, we describe a feature perfectly
+    public Clue(ClueTypes clueType, Feature feature)
+    {
+        this.clueType = clueType;
+
+
+        switch (this.clueType)
+        {
+            case ClueTypes.Useless:
+
+                break;
+            case ClueTypes.Vague:
+                break;
+            case ClueTypes.Unsure:
+                break;
+            case ClueTypes.Perfect:
+                break;
+            default:
+                break;
+        }
+    }
+
+
     public string ToFactBookString()
     {
+        string baseString = "";
+        if(clueType == ClueTypes.Useless) { return "USELESS"; };
+
+        if(clueType == ClueTypes.Vague)
+        {
+            baseString += "They are ";
+        }
+
+        if (clueType == ClueTypes.Unsure)
+        {
+            baseString += "They are either ";
+        }
+
+        if (clueType == ClueTypes.Perfect)
+        {
+            baseString += "They are definitely ";
+        }
+
+
+
         return "";
     }
 
--- Assets/CluesAndKnowledge/ClueFactory.cs
+++ Assets/CluesAndKnowledge/ClueFactory.cs
@@ -7,18 +7,117 @@ using System.Threading.Tasks;
 
 public static class ClueFactory
 {
-
-    enum ClueTypes {
-        True,
-        False,
-        Maybe,
-        Useless,
+    public enum ClueTypes
+    {
+        Useless, //No information
+        Vague, //I think they wear a hat / I think they wear something green
+        Unsure, //Either or
+        Perfect, // They wear a green bowler hat.
+        TESTING,
     }
 
-    public static Clue GenerateClueFromEmployeeAndTarget(Person Employee, Person Target)
+    private static Dictionary<ClueTypes, int> LowLevelEmployeeClueProbs = new Dictionary<ClueTypes, int>()
+    {
+        {ClueTypes.Useless, 20},
+        {ClueTypes.Vague, 10},
+        {ClueTypes.Unsure, 50},
+        {ClueTypes.Perfect, 20},
+    };
+    private static Dictionary<ClueTypes, int> ManagerEmployeeClueProbs = new Dictionary<ClueTypes, int>()
+    {
+        {ClueTypes.Useless, 20},
+        {ClueTypes.Vague, 5},
+        {ClueTypes.Unsure, 50},
+        {ClueTypes.Perfect, 25},
+    };
+    private static Dictionary<ClueTypes, int> ExecEmployeeClueProbs = new Dictionary<ClueTypes, int>()
+    {
+        {ClueTypes.Useless, 30},
+        {ClueTypes.Vague, 20},
+        {ClueTypes.Unsure, 10},
+        {ClueTypes.Perfect, 40},
+    };
+    private static Dictionary<ClueTypes, int> CEOEmployeeClueProbs = new Dictionary<ClueTypes, int>()
+    {
+        {ClueTypes.Useless, 40},
+        {ClueTypes.Vague, 0},
+        {ClueTypes.Unsure, 0},
+        {ClueTypes.Perfect, 60},
+    };
+
+    //Special Cases:
+    //Employees in the same department
+    //Manager of the employee
+
+
+    public static Clue GenerateRandomClueFromEmployeeAndTarget(Person Employee, Person Target)
     {
         // TODO: Logic here please.
-        return new Clue();
+
+        Random random = new System.Random();
+        int roll = random.Next(1, 101);
+
+        //Generating random clue type
+        ClueTypes randomClueType = ClueTypes.TESTING;
+        switch (Employee.rank)
+        {
+            case Person.Rank.Employee:
+                randomClueType = PickClueCategory(roll, LowLevelEmployeeClueProbs);
+                break;
+
+            case Person.Rank.Manager:
+                randomClueType = PickClueCategory(roll, ManagerEmployeeClueProbs);
+                break;
+
+            case Person.Rank.Executive:
+                randomClueType = PickClueCategory(roll, ExecEmployeeClueProbs);
+                break;
+
+            case Person.Rank.CEO:
+                randomClueType = PickClueCategory(roll, CEOEmployeeClueProbs);
+                break;
+
+            default:
+                break;
+        }
+
+        //if (Employee.currentLocation == Target.currentLocation)
+        //{
+        //
+        //}
+
+        return new Clue(randomClueType);
+    }
+
+    private static ClueTypes PickClueCategory(int randomRoll, Dictionary<ClueTypes, int> probabilities)
+    {
+        ClueTypes clue = ClueTypes.TESTING;
+        int totalWeight = 0;
+        foreach (KeyValuePair<ClueTypes, int> entry in probabilities)
+        {
+            totalWeight += entry.Value;
+        }
+
+
+        foreach (KeyValuePair<ClueTypes, int> entry in probabilities)
+        {
+            //If value has 0, then it is skipped.
+            if (entry.Value == 0)
+            {
+                continue;
+            }
+
+            //If randomRoll is under entry.Value, then it is under the threshold, and so takes entry.Key
+            if (randomRoll <= entry.Value)
+            {
+                clue = entry.Key;
+                break;
+            }
+
+            //Otherwise, we reduce the roll by the Value to allow the other weights to potentially be picked.
+            randomRoll -= entry.Value;
+        }
+        return clue;
     }
 
 
