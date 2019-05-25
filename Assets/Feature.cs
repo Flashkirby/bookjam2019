c1e46b79589b039fdce001198761c5c8b3aa8f29
Unknown <flashkirby@gmail.com>
2019-05-25 14:26:05 +0100

    Add Pixel Perfect Camera

---
+++ Assets/Feature.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Feature : MonoBehaviour
+    {
+        public SpriteRenderer spriteRenderer;
+
+        public void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        public void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+        void FixedUpdate()
+        {
+
+        }
+    }
+}
---
+++ Assets/Knowledge.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    /// <summary>
+    /// Class represents everything there is to know about a target.
+    /// </summary>
+    public class Knowledge
+    {
+    }
+
+    public enum Logic
+    { Unknown, True, False, Maybe }
+}
