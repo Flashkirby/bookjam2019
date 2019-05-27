c9c7d04a71505115b2116d5c3cc0277b97a52a54
Unknown <flashkirby@gmail.com>
2019-05-27 17:49:49 +0100

    Add background parallax

---
+++ Assets/BackgroundMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMove : MonoBehaviour
+{
+    public new Transform camera;
+    public Vector3 offset;
+
+    public void Awake()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        offset = transform.position - camera.position;
+    }
+
+    Vector3 oldPos;
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        transform.position = transform.position.SetX3((camera.position.x - offset.x) * (1f / transform.position.z));
+    }
+}
