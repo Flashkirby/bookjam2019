7d2019a6d3d2d9e9984b59ac65096f5de0df4fff
terapter <Terapter@users.noreply.github.com>
2019-05-27 15:52:42 +0100

    Hide room until you enter

---
+++ Assets/RoomHider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomHider : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Detective>() != null)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
